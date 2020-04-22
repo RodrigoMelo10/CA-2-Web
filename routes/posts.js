a1df201bd75e96a88637a3d01152c78bfaf3687b
RodrigoMelo10 <digoliveira10@yahoo.com.br>
2020-04-22 12:04:26 +0000

    creating middleware posts

---
+++ routes/posts.js
@@ -0,0 +1,9 @@
+const express = require ('express');
+const router = express.Router();
+router.get ('/',)
+app.get('/posts',(req,res)=> {
+
+res.send ("Posts message");
+});
+
+module.exports = router;
