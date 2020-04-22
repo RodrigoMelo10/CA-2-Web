6cb59a2f3f6faf34a60e76f4e869a13665caf52d
RodrigoMelo10 <digoliveira10@yahoo.com.br>
2020-04-22 10:37:29 +0000

    Data base is connected

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 
 const app = express();
+const mongoose = require("mongoose");
+require('dotenv/config')
 
 app.get('/',(req,res)=> {
 
 res.send ("Hello");
 
 
-})
+});
+app.get('/posts',(req,res)=> {
+
+res.send ("Posts message");
+});
+
+mongoose.connect(process.env.DB_CONNECTION,
+ {useUnifiedTopology: true, useNewUrlParser: true }, 
+()=>console.log("It is connected to DB")
+
+);
+
 
 app.listen(3000);
